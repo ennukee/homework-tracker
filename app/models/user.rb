7541873f4cd67f752b57197b0bdb05309397c839
Dylan Bowers <djbowers@umass.edu>
2015-11-09 16:06:20 -0500

    user model and password instantiated

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #if I ever add emails
+  before_save { self.username = username.downcase }
+
+  has_many :assignments
+  validates :username, presence: true, 
+                       length: { maximum: 20 },
+                       uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+                       length: { minimum: 6 }
+
+  has_secure_password
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103183328) do
+ActiveRecord::Schema.define(version: 20151109210043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,12 @@ ActiveRecord::Schema.define(version: 20151103183328) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "assignments", "users"
 end
